8c140c32dd488f23dc58b47c68ed8413a3b98da1
lilinjian <lljsg123123@gmail.com>
2022-03-15 18:15:32 -0700

    fix the gender model

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import PIL
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.layers as L
+from keras.layers import LeakyReLU
 from matplotlib import pyplot as plt
 
 from data import Data
@@ -20,25 +21,22 @@ class Model:
         self.build_model()
 
     def build_model(self):
+        print("+++++++++++++++++++++++++building age model+++++++++++++++++++++++++++++")
         self.age_model = self.get_age_model()
+        print("+++++++++++++++++++++++++building gender model+++++++++++++++++++++++++++++")
         self.gender_model = self.get_gender_model()
+        print("+++++++++++++++++++++++++building ethnicity model+++++++++++++++++++++++++++++")
         self.ethnicity_model = self.get_ethnicity_model()
 
     def get_age_model(self):
         model = tf.keras.Sequential([
             L.InputLayer(input_shape=(48, 48, 1)),
-            L.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+            L.Dense(512, activition=LeakyReLU(alpha=0.3)),
             L.BatchNormalization(),
-            L.MaxPooling2D((2, 2)),
-            L.Conv2D(64, (3, 3), activation='relu'),
-            L.MaxPooling2D((2, 2)),
-            L.Flatten(),
-            L.Dense(64, activation='relu'),
-            L.Dropout(rate=0.5),
-            L.Dense(1, activation='sigmoid')
+            L.Dropout(rate=0.2),
+            L.Dense(1, activation=None)
         ])
         self.compile_and_fit(model, self.data.ages)
-
         return model
 
     def get_gender_model(self):
@@ -51,7 +49,7 @@ class Model:
             L.MaxPooling2D((2, 2)),
             L.Flatten(),
             L.Dense(64, activation='relu'),
-            L.Dropout(rate=0.5),
+            L.Dropout(rate=0.2),
             L.Dense(1, activation='sigmoid')
         ])
         self.compile_and_fit(model, self.data.genders)
@@ -67,13 +65,13 @@ class Model:
             L.MaxPooling2D((2, 2)),
             L.Flatten(),
             L.Dense(64, activation='relu'),
-            L.Dropout(rate=0.5),
-            L.Dense(1, activation='sigmoid')
+            L.Dropout(rate=0.2),
+            L.Dense(1, activation='softmax')
         ])
         self.compile_and_fit(model, self.data.ethnicities)
         return model
 
-    def compile_and_fit(self, model, y, max_epochs=10000):
+    def compile_and_fit(self, model, y, max_epochs=2000):
         X_train, X_test, y_train, y_test = self.data.get_data(y, test_size=0.22)
         model.compile(optimizer='sgd',
                       loss='mean_squared_error',
@@ -102,6 +100,7 @@ class Model:
         age = self.age_model.predict(image_pixels)
         gender = self.gender_model.predict(image_pixels)
         ethnicity = self.ethnicity_model.predict(image_pixels)
+        print('age: {}, gender: {}, ethnicity: {}'.format(age, gender, ethnicity))
         plt.title(
             f'Prediction: {age[0][0].argmax()} years, {self.gender_classifications[gender[0][0].argmax()]}, {self.ethnicity_classifications[ethnicity[0][0].argmax()]}\n')
         plt.imshow(image_pixels)
